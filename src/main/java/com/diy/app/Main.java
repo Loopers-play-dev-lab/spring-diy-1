1f235591185fe22f0ea5e36b2ed5939692d1a410
APapeIsName <gms72901217@gmail.com>
2026-04-20 01:09:06 +0900

    Revert "feat: 예제 홈 서블릿 연결"
    This reverts commit 2c6ff988dd74357231a9a03ca771c28f0819addb.

--- src/main/java/com/diy/app/Main.java
+++ src/main/java/com/diy/app/Main.java
@@ -1,31 +1,6 @@
 package com.diy.app;
 
-import com.diy.framework.web.HomeServlet;
-import com.diy.framework.web.server.TomcatWebServer;
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
-
-import java.io.File;
-
 public class Main {
     public static void main(String[] args) {
-//        final Tomcat tomcat = new Tomcat();
-//        tomcat.setPort(8080);
-        final TomcatWebServer tomcat = new TomcatWebServer();
-
-//        try {
-            tomcat.start();
-//            final Thread awaitThread = new Thread(() -> tomcat.getServer().await());
-
-//            final Context context = tomcat.addContext("", new File(".").getAbsolutePath());
-//            tomcat.addServlet(context, "homeServlet", new HomeServlet()); // 예제에서 오타있어요 Tomcat -> tomcat
-//            context.addServletMappingDecoded("/home", "homeServlet");
-
-//            awaitThread.start();
-//        } catch (LifecycleException e) {
-//            throw new RuntimeException(e);
-//        }
-
     }
 }
