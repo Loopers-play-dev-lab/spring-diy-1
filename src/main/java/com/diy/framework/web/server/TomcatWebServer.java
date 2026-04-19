cd9fc1968f1273661df6eaf8026b2669b244543c
yuntasha <jaljayo85@gmail.com>
2026-04-19 17:37:03 +0900

    :construction: 톰캣 프레임 워크 훑어보기

--- src/main/java/com/diy/framework/web/server/TomcatWebServer.java
+++ src/main/java/com/diy/framework/web/server/TomcatWebServer.java
@@ -26,6 +26,7 @@ public class TomcatWebServer {
     public void startServerInternal() {
         try {
             tomcat.setPort(port);
+            // 스레드에서 start부터 하면 안되나?
             tomcat.start();
             final Thread awaitThread = new Thread(() -> tomcat.getServer().await());
             awaitThread.start();
@@ -35,9 +36,12 @@ public class TomcatWebServer {
     }
 
     private void setServerContext() {
+        // resource 파일 위치 가져오기
         final String resourcesPath = Paths.get("src", "main", "resources").toString();
+        // 절대 경로로 변경 (File이라 헷갈렸는데 디렉토리로 찾아지는듯 -> 리눅스는 모든 것이 파일이다라는 마인드가 있어서 적용된 것으로 보임)
         final String absoluteResourcesPath = new File(resourcesPath).getAbsolutePath();
 
+        // url 경로와 리소스 경로 매핑 (contextPath를 /abc로 바꾸면 prefix가 붙게됨)
         final Context context = this.tomcat.addWebapp("/", absoluteResourcesPath);
 
         context.setRequestCharacterEncoding("UTF-8");
@@ -49,7 +53,12 @@ public class TomcatWebServer {
     private void setServerResources(final Context context) {
         final String classPath = getClassPath();
 
+        // 발드된 리소스 file Path main까지 들어가져있음
         final StandardRoot resources = new StandardRoot(context);
+        // 내부적으로 WEB-INF/classes 디렉토리와 마운팅이 되어있다고 나오는데 톰캣에서 저 위치를 외부 통신할때 사용할 수 있도록 되어있는 것으로 예상됨
+        // internalPath는 저기중에서도 특정 위치의 애들만 가져올 수 있도록 하위 애들 가져올 수 있게 만들어지는듯
+        // /com 이렇게해도 아래 하위에 있는 애들은 전부 들어가져서 크게 문제는 없어보이긴 하는데...
+        // 질문: resources라고 build 하위에 있는 애들인데 막상 classPath는 build/classes/java/main까지 들어감 왜 그런거지....
         resources.addPostResources(new DirResourceSet(resources, "/WEB-INF/classes", classPath, "/"));
 
         context.setResources(resources);
