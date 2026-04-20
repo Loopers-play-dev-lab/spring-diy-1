c1610a16c8c9cd318e72f6e39476b5e9d3ce4753
Seyoung Hong <maiorem00@gmail.com>
2026-04-20 22:54:39 +0900

    feat: 강의 등록/조회/수정/삭제

--- src/main/java/com/diy/app/Main.java
+++ src/main/java/com/diy/app/Main.java
@@ -1,6 +1,28 @@
 package com.diy.app;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        final Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        try {
+            final Context context = tomcat.addWebapp("", new File("src/main/resources").getAbsolutePath());
+            Tomcat.addServlet(context, "lectureServlet", new LectureServlet());
+            context.addServletMappingDecoded("/lecture", "lectureServlet");
+
+            tomcat.start();
+
+            final Thread awaitThread = new Thread(() -> tomcat.getServer().await());
+            awaitThread.start();
+
+        } catch (LifecycleException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
