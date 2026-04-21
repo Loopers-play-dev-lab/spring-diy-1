95cfb9ac543996600286a03c68ec89ccea8f098a
hyunn12 <hyunn964@gmail.com>
2026-04-22 00:46:42 +0900

    feat: LectureServlet 강의 수정 구현

--- src/main/java/com/diy/app/LectureServlet.java
+++ src/main/java/com/diy/app/LectureServlet.java
@@ -19,13 +19,14 @@ public class LectureServlet  extends HttpServlet {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final List<Lecture> lectures = new ArrayList<>();
+    private Long idx = 1L;
 
     @Override
     public void init() throws ServletException {
         System.out.println("[INIT] servlet initialized");
 
         IntStream.rangeClosed(1, 5)
-                .forEach(i -> lectures.add(new Lecture(i, "TEST"+i, BigDecimal.valueOf(i*10000))));
+                .forEach(i -> lectures.add(new Lecture(idx++, "TEST"+i, BigDecimal.valueOf(i*10000))));
 
         super.init();
     }
@@ -51,7 +52,7 @@ public class LectureServlet  extends HttpServlet {
 
         Map<String, Object> payload = objectMapper.readValue(req.getReader(), Map.class);
         Lecture lecture = new Lecture(
-                lectures.size()+1,
+                idx++,
                 payload.get("name").toString(),
                 new BigDecimal(payload.get("price").toString())
         );
@@ -60,18 +61,42 @@ public class LectureServlet  extends HttpServlet {
         resp.sendRedirect("/lectures");
     }
 
+    /**
+     * 강의 수정
+     */
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("[PUT] lecture modify");
+
+        // req: id, name, price
+        Map<String, Object> payload = objectMapper.readValue(req.getReader(), Map.class);
+        Long id = Long.valueOf(payload.get("id").toString());
+
+        for (int i = 0; i < lectures.size(); i++) {
+            var orgLecture = lectures.get(i);
+            if (id.equals(orgLecture.getId())) {
+                lectures.set(i, new Lecture(
+                        orgLecture.getId(),
+                        payload.get("name").toString(),
+                        new BigDecimal(payload.get("price").toString())
+                ));
+                break;
+            }
+        }
+    }
+
     public static class Lecture {
-        private final int id;
+        private final Long id;
         private final String name;
         private final BigDecimal price;
 
-        private Lecture(int id, String name, BigDecimal price) {
+        private Lecture(Long id, String name, BigDecimal price) {
             this.id = id;
             this.name = name;
             this.price = price;
         }
 
-        public int getId() {
+        public Long getId() {
             return id;
         }
 
