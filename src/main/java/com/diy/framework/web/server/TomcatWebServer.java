b1bbee7df7045aac3f07be97a9f0f284a9f44b67
혜영 <hysung714@naver.com>
2026-04-21 14:12:04 +0000

    refactor(server): 서블릿 등록 API를 외부에서 호출 가능하도록 분리
    - start()를 prepareContext/addServlet/startServer로 분해
    - Context 필드화하여 외부에서 서블릿 추가 가능

--- src/main/java/com/diy/framework/web/server/TomcatWebServer.java
+++ src/main/java/com/diy/framework/web/server/TomcatWebServer.java
@@ -6,6 +6,7 @@ import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
 
+import javax.servlet.http.HttpServlet;
 import java.io.File;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
@@ -16,29 +17,13 @@ public class TomcatWebServer {
 
     private final Tomcat tomcat = new Tomcat();
     private final int port = 8080;
-
-    public void start() {
-        setServerContext();
-        startDaemonAwaitThread();
-        startServerInternal();
-    }
-
-    public void startServerInternal() {
-        try {
-            tomcat.setPort(port);
-            tomcat.start();
-            final Thread awaitThread = new Thread(() -> tomcat.getServer().await());
-            awaitThread.start();
-        } catch (LifecycleException e) {
-            throw new RuntimeException("톰켓 서버 실행 중 예외가 발생했습니다.", e);
-        }
-    }
+    private Context context;
 
     private void setServerContext() {
         final String resourcesPath = Paths.get("src", "main", "resources").toString();
         final String absoluteResourcesPath = new File(resourcesPath).getAbsolutePath();
 
-        final Context context = this.tomcat.addWebapp("/", absoluteResourcesPath);
+        this.context = this.tomcat.addWebapp("/", absoluteResourcesPath);
 
         context.setRequestCharacterEncoding("UTF-8");
         context.setResponseCharacterEncoding("UTF-8");
@@ -65,10 +50,25 @@ public class TomcatWebServer {
         }
     }
 
-    private void startDaemonAwaitThread() {
-        final Thread awaitThread = new Thread(() -> TomcatWebServer.this.tomcat.getServer().await());
-        awaitThread.setContextClassLoader(getClass().getClassLoader());
-        awaitThread.setDaemon(false);
-        awaitThread.start();
+    public void prepareContext() {
+        setServerContext();
+    }
+
+    public void addServlet(String urlPattern, String servletName, HttpServlet servlet) {
+        Tomcat.addServlet(this.context, servletName, servlet);
+        this.context.addServletMappingDecoded(urlPattern, servletName);
+    }
+
+    public void startServer() {
+        try {
+            tomcat.setPort(port);
+            tomcat.start();
+            Thread awaitThread = new Thread(() -> tomcat.getServer().await());
+            awaitThread.setContextClassLoader(getClass().getClassLoader());
+            awaitThread.setDaemon(false);
+            awaitThread.start();
+        } catch (LifecycleException e) {
+            throw new RuntimeException("톰캣 서버 실행 중 예외가 발생했습니다.", e);
+        }
     }
 }
