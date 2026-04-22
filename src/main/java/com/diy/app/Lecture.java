deb39af31053b0d83bfcf1d7f7bc56c276fbbd00
portlogics-anewjean <anewjean@portlogics.com>
2026-04-22 17:46:30 +0900

    feat: Lecture 도메인 모델 구현
    - 팩토리 메서드 패턴으로 생성 및 CRUD 지원

--- src/main/java/com/diy/app/Lecture.java
+++ src/main/java/com/diy/app/Lecture.java
@@ -1,4 +1,41 @@
 package com.diy.app;
 
+import java.math.BigDecimal;
+
 public class Lecture {
+    private static int seq = 0;
+
+    private int id;
+    private String name;
+    private BigDecimal price;
+
+    private Lecture(final String name, final BigDecimal price) {
+        this.id = ++seq;
+        this.name = name;
+        this.price = price;
+    }
+
+    public static Lecture register(final String name, final BigDecimal price) {
+        return new Lecture(name, price);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void updateName(final String newName) {
+        this.name = newName;
+    }
+
+    public void updatePrice(final BigDecimal newPrice) {
+        this.price = newPrice;
+    }
 }
