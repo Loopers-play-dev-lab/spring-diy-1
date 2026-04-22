48fba00e7eb51c89f8950b9e8b37eb0200cf8e1c
ksh <ksh7412.dev@gmail.com>
2026-04-22 20:34:46 +0900

    feat : 1주차 기능 구현
    - 강의 등록(POST, /lectures)
    - 강의 수정(PUT, /lectures)
    - 강의 삭제(DELETE, /lectures)
    - 강의 목록(GET, /lectures)

---
+++ src/main/java/com/diy/app/entity/Lecture.java
@@ -0,0 +1,35 @@
+package com.diy.app.entity;
+
+import java.math.BigDecimal;
+
+public class Lecture {
+    static Long lectureId = 1L;
+
+    final Long id;
+    final String name;
+    final BigDecimal price;
+
+    public Lecture(Long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Lecture(String name, BigDecimal price) {
+        this.name = name;
+        this.id = lectureId++;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
