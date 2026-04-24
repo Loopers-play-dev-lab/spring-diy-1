143854c0310b6ef6b1e58fcdbb2f29c8e7ac37e8
won <dlwnddnjs96@gmail.com>
2026-04-24 14:43:11 +0900

    feat: 요구사항 범위 먼저 구현 및 WebTomcatServer 사용하도록 수정

--- src/main/java/com/diy/app/servlet/LectureServlet.java
+++ src/main/java/com/diy/app/servlet/LectureServlet.java
@@ -1,6 +1,7 @@
 package com.diy.app.servlet;
 
 import com.diy.app.domain.Lecture;
+import com.diy.app.repository.InMemoryLectureRepositoryImpl;
 import com.diy.app.repository.LectureRepository;
 import com.diy.app.servlet.dto.request.LectureDelete;
 import com.diy.app.servlet.dto.request.LecturePost;
@@ -10,20 +11,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.List;
 
+@WebServlet("/lectures")
 public class LectureServlet extends HttpServlet {
-    private final LectureRepository lectureRepository;
+    private final LectureRepository lectureRepository = new InMemoryLectureRepositoryImpl();
     private final ObjectMapper objectMapper = new ObjectMapper();
-
-    public LectureServlet(LectureRepository lectureRepository) {
-        this.lectureRepository = lectureRepository;
-    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -34,15 +32,8 @@ public class LectureServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        BufferedReader reader = req.getReader();
-        StringBuilder builder = new StringBuilder();
-        while(reader.ready()){
-            builder.append(reader.readLine());
-        }
-
-        String messageBody = builder.toString();
-        LecturePost postRequest = objectMapper.readValue(messageBody, LecturePost.class);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        LecturePost postRequest = objectMapper.readValue(req.getInputStream(), LecturePost.class);
         Lecture lecture = Lecture.makeForSave(postRequest);
         lectureRepository.save(lecture);
         resp.setStatus(HttpServletResponse.SC_SEE_OTHER);
@@ -50,15 +41,8 @@ public class LectureServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        BufferedReader reader = req.getReader();
-        StringBuilder builder = new StringBuilder();
-        while(reader.ready()){
-            builder.append(reader.readLine());
-        }
-
-        String messageBody = builder.toString();
-        LectureUpdate updateRequest = objectMapper.readValue(messageBody, LectureUpdate.class);
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        LectureUpdate updateRequest = objectMapper.readValue(req.getInputStream(), LectureUpdate.class);
         Lecture updateLecture = new Lecture(updateRequest.id(), updateRequest.name(), updateRequest.price());
         lectureRepository.updateById(updateRequest.id(), updateLecture);
         resp.setStatus(HttpServletResponse.SC_SEE_OTHER);
@@ -66,16 +50,9 @@ public class LectureServlet extends HttpServlet {
     }
 
     @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        BufferedReader reader = req.getReader();
-        StringBuilder builder = new StringBuilder();
-        while(reader.ready()){
-            builder.append(reader.readLine());
-        }
-
-        String messageBody = builder.toString();
-        LectureDelete updateRequest = objectMapper.readValue(messageBody, LectureDelete.class);
-        lectureRepository.deleteById(updateRequest.id());
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        LectureDelete deleteRequest = objectMapper.readValue(req.getInputStream(), LectureDelete.class);
+        lectureRepository.deleteById(deleteRequest.id());
         resp.setStatus(HttpServletResponse.SC_SEE_OTHER);
         resp.setHeader("Location", "/lectures");
     }
