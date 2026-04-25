70571c2c77c29fa01184e5423e9e35556d8a94e0
APapeIsName <gms72901217@gmail.com>
2026-04-25 22:42:47 +0900

    feat: LectureController 생성 및 Controller 구현하도록

---
+++ src/main/java/com/diy/app/controller/LectureController.java
@@ -0,0 +1,38 @@
+package com.diy.app.controller;
+
+import com.diy.framework.web.ModelAndView;
+import com.diy.framework.web.controller.Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LectureController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+        switch (req.getMethod()) {
+            case "POST" -> {
+                return doPost(req, res);
+            }
+            case "GET" -> {
+                return doGet(req, res);
+            }
+            default -> {
+                throw new RuntimeException("404 Not Found");
+            }
+        }
+
+    }
+
+    private ModelAndView doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        return new ModelAndView("redirect:/lectures");
+    }
+
+    private ModelAndView doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        return new ModelAndView("lecture-list", model);
+    }
+}
--- src/main/java/com/diy/framework/web/controller/Controller.java
+++ src/main/java/com/diy/framework/web/controller/Controller.java
@@ -1,10 +1,12 @@
 package com.diy.framework.web.controller;
 
+import com.diy.framework.web.ModelAndView;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @FunctionalInterface
 public interface Controller {
 
-    void handleRequest(final HttpServletRequest req, final HttpServletResponse rep) throws Exception;
+    ModelAndView handleRequest(final HttpServletRequest req, final HttpServletResponse res) throws Exception;
 }
