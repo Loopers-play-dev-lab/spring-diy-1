7242148d3f19cd87cbed91707a950b32462e1766
shAn-kor <kjn0406@naver.com>
2026-04-25 15:06:48 +0900

    feat: 프론트 컨트롤러 DispatcherServlet 도입

---
+++ src/main/java/com/diy/framework/web/servlet/DispatcherServlet.java
@@ -0,0 +1,16 @@
+package com.diy.framework.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/")
+public class DispatcherServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("DispatcherServlet service");
+    }
+}
