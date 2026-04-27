bca2c5ce823c62ebc5c99e616ab5f593da4255c5
APapeIsName <gms72901217@gmail.com>
2026-04-28 02:02:08 +0900

    refactor: String -> enum HttpMethod 로 변경

--- src/main/java/com/diy/app/controller/LectureController.java
+++ src/main/java/com/diy/app/controller/LectureController.java
@@ -1,8 +1,10 @@
 package com.diy.app.controller;
 
+import com.diy.app.controller.dto.OpenLectureRequest;
 import com.diy.app.domain.Lecture;
 import com.diy.app.domain.Price;
 import com.diy.app.repository.LectureRepository;
+import com.diy.framework.web.HttpMethod;
 import com.diy.framework.web.ModelAndView;
 import com.diy.framework.web.controller.Controller;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -18,24 +20,28 @@ import java.util.*;
 import static com.diy.app.controller.dto.LectureListResponse.*;
 
 public class LectureController implements Controller {
+
     private final LectureRepository lectureRepository;
+    private final ObjectMapper objectMapper;
 
     public LectureController(LectureRepository lectureRepository) {
         this.lectureRepository = lectureRepository;
+        this.objectMapper = new ObjectMapper();
     }
 
     @Override
     public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-        switch (req.getMethod()) {
-            case "POST" -> {
+        // TODO: 객체지향적으로 변경해보기
+        switch (HttpMethod.from(req.getMethod())) {
+            case POST -> {
                 return doPost(req, res);
             }
-            case "GET" -> {
+            case GET -> {
                 return doGet(req, res);
             }
             default -> {
-                throw new RuntimeException("404 Not Found");
+                throw new RuntimeException("405 Method Not Allowed");
             }
         }
 
@@ -50,18 +56,18 @@ public class LectureController implements Controller {
     }
 
     private ModelAndView doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        final Map<String, String> reqMap = parseParams(req);
+        final String body = new String(req.getInputStream().readAllBytes());
+        final OpenLectureRequest openLectureRequest = objectMapper.readValue(body, OpenLectureRequest.class);
 
         lectureRepository.save(Lecture.open(
-                reqMap.get("name"),
-                Price.of(Long.parseLong(reqMap.get("price")))
+                openLectureRequest.name(),
+                Price.of(openLectureRequest.price())
         ));
 
         return new ModelAndView("redirect:/lectures");
     }
 
     private <T> Map<String, T> parseParams(final HttpServletRequest req) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
         if ("application/json".equals(req.getHeader("Content-Type"))) {
             final byte[] bodyBytes = req.getInputStream().readAllBytes();
             final String body = new String(bodyBytes, StandardCharsets.UTF_8);
