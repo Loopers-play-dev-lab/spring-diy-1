d4eb8b77ee05b88ba570dd67f11fef45c5feb83d
APapeIsName <gms72901217@gmail.com>
2026-04-27 10:22:05 +0900

    feat: JspViewResolver 구현

--- src/main/java/com/diy/framework/web/viewresolver/JspViewResolver.java
+++ src/main/java/com/diy/framework/web/viewresolver/JspViewResolver.java
@@ -1,11 +1,21 @@
 package com.diy.framework.web.viewresolver;
 
+import com.diy.framework.web.view.HtmlView;
+import com.diy.framework.web.view.JspView;
 import com.diy.framework.web.view.View;
 
 public class JspViewResolver implements ViewResolver {
 
     @Override
     public View resolveViewName(String viewName) {
-        return null;
+        if (viewName.contains("redirect:")) {
+            return new JspView(viewName);
+        }
+
+        String extensionName = viewName.split("\\.")[1];
+        if (!extensionName.equals("jsp")) {
+            throw new RuntimeException("500 Internal Server Error");
+        }
+        return new JspView(viewName);
     }
 }
