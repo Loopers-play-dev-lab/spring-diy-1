2ab71997d6334be04969b272fa0c52a92d80c47f
won <dlwnddnjs96@gmail.com>
2026-04-28 01:04:26 +0900

    feat: LectureController 통합

--- src/main/java/com/diy/app/controller/AbstractController.java
+++
@@ -1,9 +0,0 @@
-package com.diy.app.controller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@FunctionalInterface
-public interface AbstractController {
-    void handleRequest(final HttpServletRequest request, final HttpServletResponse response) throws Exception;
-}
