0e3c858d766e57d1eca8960f2f1c1946cb13f9a3
yuntasha <jaljayo85@gmail.com>
2026-04-29 17:38:44 +0900

    :recycle: import 제거

--- src/main/java/com/diy/app/infra/viewRender/ViewResolver.java
+++ src/main/java/com/diy/app/infra/viewRender/ViewResolver.java
@@ -6,8 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
