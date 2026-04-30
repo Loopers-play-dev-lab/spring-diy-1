670a24d4e3a4a1e6ce3e1ef3c6e9c7cd59cb2eaa
yuntasha <jaljayo85@gmail.com>
2026-04-30 16:50:58 +0900

    :sparkles: getter 추가

--- src/test/java/study/reflection/Car.java
+++ src/test/java/study/reflection/Car.java
@@ -5,6 +5,14 @@ public class Car {
     private String name;
     private int price;
 
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
     public Car() {
 
     }
