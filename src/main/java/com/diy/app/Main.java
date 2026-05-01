da39f2e7e5f9d3a9823ff13b16f48f49caddc200
yuntasha <jaljayo85@gmail.com>
2026-05-01 14:48:13 +0900

    :sparkles: 컴포넌트 레포지토리 생성

--- src/main/java/com/diy/app/Main.java
+++ src/main/java/com/diy/app/Main.java
@@ -1,11 +1,13 @@
 package com.diy.app;
 
+import com.diy.app.business.repository.LectureRepository;
 import com.diy.framework.beans.factory.BeanStorage;
 import com.diy.framework.web.server.TomcatWebServer;
 
 public class Main {
     public static void main(String[] args) {
         BeanStorage beanStorage = new BeanStorage();
+        beanStorage.getBeans(LectureRepository.class).forEach(System.out::println);
         TomcatWebServer tomcatWebServer = new TomcatWebServer();
         tomcatWebServer.start();
     }
--- src/main/java/com/diy/framework/beans/factory/BeanStorage.java
+++ src/main/java/com/diy/framework/beans/factory/BeanStorage.java
@@ -2,12 +2,38 @@ package com.diy.framework.beans.factory;
 
 import com.diy.framework.beans.annotations.Component;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 import java.util.Set;
 
 public class BeanStorage {
+
+    private final List<?> beans;
+
     public BeanStorage() {
         BeanScanner bc = new BeanScanner("com.diy.app");
         Set<Class<?>> classes = bc.scanClassesTypeAnnotatedWith(Component.class);
-        classes.forEach(System.out::println);
+        beans = classes.stream().map(clazz -> {
+            Constructor<?> constructor = null;
+            try {
+                constructor = clazz.getDeclaredConstructor();
+                constructor.setAccessible(true);
+                return constructor.newInstance();
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (constructor != null) {
+                    constructor.setAccessible(false);
+                }
+            }
+        }).toList();
+    }
+
+    public <T> List<T> getBeans(final Class<T> classType) {
+        return beans.stream()
+                .filter(classType::isInstance)
+                .map(classType::cast)
+                .toList();
     }
 }
