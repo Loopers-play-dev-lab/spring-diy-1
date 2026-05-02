2b181640055b5d4553cbf5c5c5dc2876b9d3d06e
김용권 <yongkwon.kim@ikoob.com>
2026-05-02 23:47:49 +0900

    fix : 요구사항2 수정(메소드 이름 출력이 아닌 메소드 실행하도록 수정)

--- src/test/java/study/reflection/ReflectionTest.java
+++ src/test/java/study/reflection/ReflectionTest.java
@@ -8,23 +8,24 @@ import org.junit.jupiter.api.Test;
 public class ReflectionTest {
 
     @Test
-    @DisplayName("Car 객체 정보 가져오기")
+    @DisplayName("요구사항1 -> Car 객체 정보 가져오기")
     void showClass() {
         Class<Car> carClass = Car.class;
         System.out.println(carClass.getName());
     }
 
     @Test
-    @DisplayName("Car test로 시작하는 메소드를 자동 실행하기")
+    @DisplayName("요구사항2 -> Car test로 시작하는 메소드를 자동 실행하기")
     void testMethodRun()
-        throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
         Class<Car> clazz = Car.class;
 
         Method[] declaredMethods = clazz.getDeclaredMethods();
         for (Method declaredMethod : declaredMethods) {
 
             if(declaredMethod.getName().startsWith("test")) {
-                System.out.println(declaredMethod.getName());
+                Car car = clazz.getDeclaredConstructor().newInstance();
+                System.out.println(declaredMethod.invoke(car));
             }
         }
     }
