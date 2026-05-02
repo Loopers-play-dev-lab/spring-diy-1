cc7be2258baabb5c09dc4861c16ae23ef2f3735c
김용권 <yongkwon.kim@ikoob.com>
2026-05-02 23:30:30 +0900

    feat : 요구사항2 구현

--- src/test/java/study/reflection/ReflectionTest.java
+++ src/test/java/study/reflection/ReflectionTest.java
@@ -1,5 +1,7 @@
 package study.reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +12,20 @@ public class ReflectionTest {
     void showClass() {
         Class<Car> carClass = Car.class;
         System.out.println(carClass.getName());
+    }
+
+    @Test
+    @DisplayName("Car test로 시작하는 메소드를 자동 실행하기")
+    void testMethodRun()
+        throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<Car> clazz = Car.class;
+
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method declaredMethod : declaredMethods) {
+
+            if(declaredMethod.getName().startsWith("test")) {
+                System.out.println(declaredMethod.getName());
+            }
+        }
     }
 }
