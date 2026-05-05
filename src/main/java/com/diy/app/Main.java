7596a006b03f5b8503e2ecd64bb4320a64d54803
Kimchuljoong <wnmh123@gmail.com>
2026-05-06 01:53:12 +0900

    불필요한 예외 signature 제거

--- src/main/java/com/diy/app/Main.java
+++ src/main/java/com/diy/app/Main.java
@@ -3,7 +3,7 @@ package com.diy.app;
 import com.diy.framework.web.server.TomcatWebServer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         final TomcatWebServer tomcat = new TomcatWebServer();
         tomcat.start();
