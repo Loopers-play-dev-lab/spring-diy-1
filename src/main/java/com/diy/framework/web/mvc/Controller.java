b18aabd3f36fbee9897b79ef4b3d3c1d94bde97b
dnya0 <annay01794@gmail.com>
2026-05-06 02:51:50 +0900

    feat: 2주차 웹 요청 추상화

---
+++ src/main/java/com/diy/framework/web/mvc/Controller.java
@@ -0,0 +1,10 @@
+package com.diy.framework.web.mvc;
+
+import com.diy.framework.web.mvc.view.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Controller {
+    ModelAndView handleRequest(final HttpServletRequest request, final HttpServletResponse response) throws Exception;
+}
