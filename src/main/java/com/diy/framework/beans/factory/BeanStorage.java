f1dcf506bb4525fcf7fefb0d8d3d516017ede79b
yuntasha <jaljayo85@gmail.com>
2026-05-05 23:29:36 +0900

    :sparkles: 위상정렬을 통한 만드는 순서 조정 및 예외처리

--- src/main/java/com/diy/framework/beans/factory/BeanStorage.java
+++ src/main/java/com/diy/framework/beans/factory/BeanStorage.java
@@ -11,13 +11,14 @@ public class BeanStorage {
 
     private static BeanStorage instance;
 
-    private final List<?> beans;
+    private final List<Object> beans;
     private final BeanScanner bc = new BeanScanner("com.diy.app");
 
     private BeanStorage() {
         Set<Class<?>> classes = bc.scanClassesTypeAnnotatedWith(Component.class);
-        beans = getBeans(classes);
-        setFields(getFields());
+        Map<Class<?>, Set<Field>> classToFields = getFields(classes);
+        List<Class<?>> sortedClasses = topologicalSort(classToFields);
+        beans = makeBeans(sortedClasses, classToFields);
     }
 
     public static BeanStorage getInstance() {
@@ -25,13 +26,17 @@ public class BeanStorage {
         return instance;
     }
 
-    private static List<?> getBeans(Set<Class<?>> classes) {
-        return classes.stream().map(clazz -> {
+    private static List<Object> makeBeans(List<Class<?>> classes, Map<Class<?>, Set<Field>> classToFields) {
+        List<Object> results = new ArrayList<>();
+
+        for (Class<?> clazz : classes) {
             Constructor<?> constructor = null;
             try {
                 constructor = clazz.getDeclaredConstructor();
                 constructor.setAccessible(true);
-                return constructor.newInstance();
+                Object bean = constructor.newInstance();
+                setFields(bean, results, classToFields);
+                results.add(bean);
             } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
                      NoSuchMethodException e) {
                 throw new RuntimeException(e);
@@ -40,7 +45,9 @@ public class BeanStorage {
                     constructor.setAccessible(false);
                 }
             }
-        }).toList();
+        }
+
+        return results;
     }
 
     public <T> List<T> getBeans(final Class<T> classType) {
@@ -50,40 +57,67 @@ public class BeanStorage {
                 .toList();
     }
 
-    public Map<Class<?>, Set<Field>> getFields() {
+    public Map<Class<?>, Set<Field>> getFields(Set<Class<?>> classes) {
         Map<Class<?>, Set<Field>> classToField = new HashMap<>();
-        for (Class<?> clazz : beans.stream().map(Object::getClass).toList()) {
+        for (Class<?> clazz : classes) {
             classToField.put(clazz, bc.scanField(clazz));
         }
         return classToField;
     }
 
-    public void setFields(Map<Class<?>, Set<Field>> classToFields) {
-        for (Object bean : beans) {
-            for (Field field : classToFields.get(bean.getClass())) {
-                Object target = null;
-                for (Object targetBean : beans) {
-                    if (!field.getType().isInstance(targetBean)) continue;
-                    if (Objects.nonNull(target)) throw new IllegalArgumentException("2개 이상의 빈이 존재");
-                    target = targetBean;
-                }
-                if (Objects.isNull(target)) throw new IllegalArgumentException("존재하지 않는 빈" + field.getName());
-                try {
-                    field.setAccessible(true);
-                    field.set(bean, target);
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                } finally {
-                    field.setAccessible(false);
-                }
+    public static void setFields(Object target, List<Object> nowBeanList, Map<Class<?>, Set<Field>> classToFields) {
+        for (Field field : classToFields.get(target.getClass())) {
+            Object fieldTarget = null;
+            for (Object targetBean : nowBeanList) {
+                if (!field.getType().isInstance(targetBean)) continue;
+                if (Objects.nonNull(fieldTarget)) throw new IllegalArgumentException("2개 이상의 빈이 존재 : " + field.getType().getName());
+                fieldTarget = targetBean;
+            }
+            if (Objects.isNull(fieldTarget)) throw new IllegalArgumentException("존재하지 않는 빈 : " + field.getType().getName());
+            try {
+                field.setAccessible(true);
+                field.set(target, fieldTarget);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } finally {
+                field.setAccessible(false);
             }
         }
     }
 
-    // 그냥 자기 필드에 있는 애들 쭉 순회하면서 자기 자신으로 돌아오는지 확인
-    // 그러면서 해당 인스턴스가 아닌 애들이 존재하면 탈락
-    // 순환 참조
-//    public boolean checkValidation(Map<Class<?>, Set<Field>> classMap) {
-//        Set<Class<?>>
-//    }
+    public List<Class<?>> topologicalSort(Map<Class<?>, Set<Field>> classMap) {
+        Map<Class<?>, Set<Class<?>>> map = copyMap(classMap);
+
+        List<Class<?>> result = new ArrayList<>();
+        Set<Class<?>> visited = new HashSet<>();
+
+        while (visited.size() < map.size()) {
+            List<Class<?>> canMake = new ArrayList<>();
+            for (Map.Entry<Class<?>, Set<Class<?>>> entry : map.entrySet()) {
+                if (visited.contains(entry.getKey())) continue;
+                if (visited.containsAll(entry.getValue())) {
+                    canMake.add(entry.getKey());
+                }
+            }
+            if (canMake.isEmpty()) {
+                throw new IllegalArgumentException("순환 참조 혹은 존재하지 않는 빈을 필드로하는 것이 존재합니다");
+            }
+            visited.addAll(canMake);
+            result.addAll(canMake);
+        }
+
+        return result;
+    }
+
+    private static Map<Class<?>, Set<Class<?>>> copyMap(Map<Class<?>, Set<Field>> classMap) {
+        Map<Class<?>, Set<Class<?>>> map = new HashMap<>();
+        for (Map.Entry<Class<?>, Set<Field>> entry : classMap.entrySet()) {
+            Set<Class<?>> fieldTypes = new HashSet<>();
+            for (Field field : entry.getValue()) {
+                fieldTypes.add(field.getType());
+            }
+            map.put(entry.getKey(), fieldTypes);
+        }
+        return map;
+    }
 }
