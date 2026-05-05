aab6a118e0a7415a8565244d8d83c519edf74328
김용권 <yongkwon.kim@ikoob.com>
2026-05-05 11:17:41 +0900

    feat : Component 애노테이션을 가지고 있는 클래스 조회 후 인스턴스 생성

--- src/main/java/com/diy/app/LectureApplication.java
+++ src/main/java/com/diy/app/LectureApplication.java
@@ -1,14 +1,28 @@
 package com.diy.app;
 
+import com.diy.framework.web.beans.annotations.Component;
+import com.diy.framework.web.beans.factory.BeanScanner;
 import com.diy.framework.web.mvc.Controller;
 import com.diy.framework.web.server.TomcatWebServer;
 import com.diy.framework.web.servlet.DispatcherServlet;
-
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class LectureApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+        throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        BeanScanner beanScanner = new BeanScanner("com.diy.app");
+        Set<Class<?>> classes = beanScanner.scanClassesTypeAnnotatedWith(Component.class);
+        for (Class<?> clazz : classes) {
+
+            if(clazz.equals(LectureRepository.class)) {
+                LectureRepository lectureRepository = (LectureRepository) clazz.getDeclaredConstructor().newInstance();
+            }
+            System.out.println(clazz.getName());
+        }
+
         final Map<String, Controller> controllerMapping = new HashMap<>();
         controllerMapping.put("/lectures", new LectureController());
 
---
+++ src/main/java/com/diy/app/LectureRepository.java
@@ -0,0 +1,10 @@
+package com.diy.app;
+
+import com.diy.framework.web.beans.annotations.Component;
+
+@Component
+public class LectureRepository {
+
+    public LectureRepository() {
+    }
+}
