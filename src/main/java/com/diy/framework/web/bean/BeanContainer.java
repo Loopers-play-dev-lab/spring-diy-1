e89a7828c0f3bbc5a13c9ae3c7db605e07589694
won <dlwnddnjs96@gmail.com>
2026-05-06 15:48:35 +0900

    refactor: 각 분기마다 빈 생성 로직 수정

--- src/main/java/com/diy/framework/web/bean/BeanContainer.java
+++ src/main/java/com/diy/framework/web/bean/BeanContainer.java
@@ -4,11 +4,9 @@ import com.diy.framework.web.bean.annotation.Autowired;
 import com.diy.framework.web.bean.annotation.Component;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class BeanContainer {
@@ -30,10 +28,17 @@ public class BeanContainer {
      */
     private void initializingContainer() {
         Set<Class<?>> classes = beanScanner.scanClassesTypeAnnotatedWith(Component.class);
-        classes.forEach(this::createBeanWithClassType);
+        classes.forEach(clazz -> {
+            Field[] declaredFields = clazz.getDeclaredFields();
+            Arrays.stream(declaredFields).forEach(field -> {
+                if (field.getType().equals(clazz)) throw new RuntimeException("circular dependency found");
+            });
+
+        });
+        classes.forEach(clazz -> createBeanWithClassType(classes, clazz));
     }
 
-    private void createBeanWithClassType(Class<?> clazz) {
+    private void createBeanWithClassType(Set<Class<?>> classes, Class<?> clazz) {
         try {
             Constructor<?>[] constructors = clazz.getConstructors();
             Constructor<?> targetConstructor = null;
@@ -53,7 +58,8 @@ public class BeanContainer {
             }
 
             if (targetConstructor.getParameterCount() == 0) {
-                container.put(clazz.getName(), targetConstructor.newInstance());
+                if (container.containsKey(clazz.getSimpleName())) return;
+                container.put(clazz.getSimpleName(), targetConstructor.newInstance());
             } else {
                 Class<?>[] parameterTypes = targetConstructor.getParameterTypes();
                 int parameterCount = targetConstructor.getParameterCount();
@@ -61,14 +67,40 @@ public class BeanContainer {
                 for (int i = 0; i < parameterCount; i++) {
                     final int index = i;
                     Class<?> parameterClass = parameterTypes[i];
-                    findBean(parameterClass)
-                            .ifPresentOrElse(bean -> parameters[index] = bean,
-                                    () -> {
-                                        createBeanWithClassType(parameterClass);
-                                        parameters[index] = getBean(parameterClass);
-                                    });
+                    if (targetConstructor.getParameters()[i].isAnnotationPresent(Autowired.class)) {
+                        String name = targetConstructor.getParameters()[i].getAnnotation(Autowired.class).beanValue();
+                        findBean(name).ifPresentOrElse(bean -> parameters[index] = bean,
+                                () -> {
+                                    for (Class ele : classes) {
+                                        if (ele.getSimpleName().equals(name)) {
+                                            createBeanWithClassType(classes, ele);
+                                        }
+                                    }
+
+                                    parameters[index] = getBean(name);
+                                });
+                    } else {
+                        findBean(parameterClass)
+                                .ifPresentOrElse(bean -> parameters[index] = bean,
+                                        () -> {
+                                            List<Class<?>> list = classes.stream()
+                                                    .filter(parameterClass::isAssignableFrom)
+                                                    .toList();
+
+                                            if (list.size() > 1) {
+                                                throw new RuntimeException(parameterClass + " constructor parameters are more than two");
+                                            } else if (list.isEmpty()) {
+                                                throw new RuntimeException(parameterClass + " no matched parameter bean found");
+                                            }
+
+                                            Class<?> targetClass = list.getFirst();
+                                            createBeanWithClassType(classes, targetClass);
+                                            parameters[index] = getBean(targetClass);
+                                        });
+                    }
                 }
-                container.put(clazz.getName(), targetConstructor.newInstance(parameters));
+                if (container.containsKey(clazz.getSimpleName())) return;
+                container.put(clazz.getSimpleName(), targetConstructor.newInstance(parameters));
             }
         } catch (InvocationTargetException | InstantiationException |
                  IllegalAccessException e) {
@@ -78,23 +110,38 @@ public class BeanContainer {
 
     public <T> T getBean(Class<T> clazz) {
         return findBean(clazz)
-                .orElseThrow(() -> new IllegalArgumentException("no matched Bean Found"));
-    }
-
-    public <T> Optional<T> findBean(Class<T> clazz) {
-        return container.values()
-                .stream()
-                .filter(clazz::isInstance)
-                .map(clazz::cast)
-                .findFirst();
+                .orElseThrow(() -> new RuntimeException("no matched Bean Found class: " + clazz ));
     }
 
     public Object getBean(String name) {
-        Object bean = container.get(name);
-        if (Objects.isNull(bean)) {
-            throw new IllegalArgumentException(name + " bean is not found");
+        return findBean(name)
+                .orElseThrow(() -> new RuntimeException("no matched Bean Found name: " + name));
+    }
+
+    public <T> Optional<T> findBean(Class<T> clazz) {
+        List<T> beanList = container.values()
+                .stream()
+                .filter(clazz::isInstance)
+                .map(clazz::cast)
+                .toList();
+
+        if (beanList.isEmpty()) {
+            return Optional.empty();
         }
 
-        return bean;
+        if (beanList.size() == 1) {
+            return Optional.of(beanList.getFirst());
+        }
+
+        throw new RuntimeException("more than two bean candidates are found");
+    }
+
+    public Optional<Object> findBean(String name) {
+        Object bean = container.get(name);
+        if (Objects.isNull(bean)) {
+            return Optional.empty();
+        }
+
+        return Optional.of(bean);
     }
 }
