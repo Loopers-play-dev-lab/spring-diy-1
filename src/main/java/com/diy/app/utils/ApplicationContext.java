04e630a6f54b295cecb84b568567dbea03f12d8f
안형준 <wns1265@gmail.com>
2026-05-07 02:27:01 +0900

    refactor: ApplicationContext 실행 흐름 최적화 및 책임 분리

--- src/main/java/com/diy/app/utils/ApplicationContext.java
+++ src/main/java/com/diy/app/utils/ApplicationContext.java
@@ -19,54 +19,18 @@ public class ApplicationContext {
         BeanFactory factory = new BeanFactory();
         DependencyInjector injector = new DependencyInjector(factory);
 
-        // 1. 클래스 스캔 및 빈 생성
+        // 1. 컴포넌트 스캔 및 빈 생성 및 등록
         Set<Class<?>> componentClasses = beanScanner.scanClassesTypeAnnotatedWith(Component.class);
-        try {
-            for (Class<?> clazz : componentClasses) {
-
-                // @Autowired가 없는 클래스 먼저 생성
-                boolean hasAutowired = Arrays.stream(clazz.getDeclaredFields()).anyMatch(field -> field.isAnnotationPresent(Autowired.class));
-                if(!hasAutowired) {
-                    try {
-                        Constructor<?> defaultConstructor = clazz.getDeclaredConstructor();
-                        defaultConstructor.setAccessible(true);
-                        Object instance = defaultConstructor.newInstance();
-                        factory.registerBean(clazz.getName(), instance);
-                        for (Class<?> iface : clazz.getInterfaces()) {
-                            // 인터페이스 이름으로 등록 (com.diy.app.repo.ILectureRepository)
-                            factory.registerBean(iface.getName(), instance);
-                        }
-                        defaultConstructor.setAccessible(false);
-                    } catch (NoSuchMethodException e) {
-                        System.out.println("기본 생성자 없음, 생성자 주입 대기중: "+clazz.getName());
-                    }
-                }
-            }
-            ApplicationContext.beanFactory = factory;
-            // 2. 생성자 스캔 및 의존성 주입
-            Set<Constructor<?>> constructors = beanScanner.scanConstructorTypeAnnotatedWith(Autowired.class);
-            for(Constructor<?> constructor : constructors) {
-                Class<?> clazz = constructor.getDeclaringClass();
-
-                // 생성자 파라미터 타입 조회
-                Object[] args = Arrays.stream(constructor.getParameterTypes())
-                        .map(type -> factory.getBean(type.getName()))
-                        .toArray();
-                constructor.setAccessible(true);
-                Object instance = constructor.newInstance(args);
-                factory.registerBean(clazz.getName(), instance);
-                for(Class<?> iface : clazz.getInterfaces()) {
-                    factory.registerBean(iface.getName(), instance);
-                }
-                constructor.setAccessible(false);
-            }
-            ApplicationContext.beanFactory = factory;
-
-//            Set<Field> autowiredFields = beanScanner.scanFieldTypeAnnotatedWith(Autowired.class);
-//            injector.inject(autowiredFields);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        factory.createBean(componentClasses);
+        ApplicationContext.beanFactory = factory;
+        // 2. 생성자 스캔 및 의존성 주입
+        Set<Constructor<?>> constructors = beanScanner.scanConstructorTypeAnnotatedWith(Autowired.class);
+        injector.constructorInject(constructors);
+        ApplicationContext.beanFactory = factory;
+        // 3. 필드 스캔 및 의존성 주입
+        Set<Field> fields = beanScanner.scanFieldTypeAnnotatedWith(Autowired.class);
+        injector.fieldInject(fields);
+        ApplicationContext.beanFactory = factory;
     }
 
     public static Object getBean(String name) {
