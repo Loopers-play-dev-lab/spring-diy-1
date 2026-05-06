1a7081a6cedfed23c1f98da462fc4e5fc6a100cd
jude-loopers <jude.dev.official@gmail.com>
2026-05-07 01:30:28 +0900

    3주차 빈생성

--- src/main/java/com/diy/app/LectureApplication.java
+++ src/main/java/com/diy/app/LectureApplication.java
@@ -1,5 +1,6 @@
 package com.diy.app;
 
+import com.diy.framework.context.ApplicationContext;
 import com.diy.framework.web.mvc.Controller;
 import com.diy.framework.web.server.TomcatWebServer;
 import com.diy.framework.web.servlet.DispatcherServlet;
@@ -11,6 +12,9 @@ public class LectureApplication {
     public static void main(String[] args) {
         final Map<String, Controller> controllerMapping = new HashMap<>();
         controllerMapping.put("/lectures", new LectureController());
+
+        final ApplicationContext ac = new ApplicationContext(LectureApplication.class.getPackageName());
+        ac.initialize();
 
         final DispatcherServlet servlet = new DispatcherServlet(controllerMapping);
         final TomcatWebServer tomcatWebServer = new TomcatWebServer(servlet);
