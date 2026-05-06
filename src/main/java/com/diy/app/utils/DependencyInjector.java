4933df92ad9c608d36bc81b9a43ffc41d96018d9
안형준 <wns1265@gmail.com>
2026-05-07 02:26:10 +0900

    refactor: 의존성 주입 메서드 네이밍 컨벤션 수정 및 로직 개선

--- src/main/java/com/diy/app/utils/DependencyInjector.java
+++ src/main/java/com/diy/app/utils/DependencyInjector.java
@@ -4,7 +4,10 @@ import com.diy.app.annotation.Autowired;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class DependencyInjector {
     private final BeanFactory beanFactory;
@@ -13,13 +16,13 @@ public class DependencyInjector {
         this.beanFactory = beanFactory;
     }
 
-    public void field_inject(Set<Field> fields) {
+    // 필드 주입 방법
+    public void fieldInject(Set<Field> fields) {
         for (Field field : fields) {
             try {
                 // 주입 대상 (어디에 꽂을 것인가)
                 Object target = beanFactory.getBean(field.getDeclaringClass().getName());
                 // 주입될 값 (무엇을 꽂을 것인가)
-                // DependencyInjector 내부
                 Object value = beanFactory.getBean(field.getType().getName());
 
                 if (target != null && value != null) {
@@ -27,28 +30,29 @@ public class DependencyInjector {
                     field.set(target, value);
                     field.setAccessible(false);
                 }
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
+            } catch (Exception e) {
+                throw new IllegalStateException(field.getDeclaringClass() + field.getName() + "필드 주입 중 오류 발생: " + e.getMessage(), e);
             }
         }
     }
-    public void constructor_inject(Set<Constructor<?>> constructors) {
-        for(Constructor<?> constructor : constructors) {
-            try{
-                // Autowired가 붙은 생성자인지 확인
-                // 주입 대상 (어디에 꽂을 것인가)
-                Object target = beanFactory.getBean(constructor.getDeclaringClass().getName());
-                // 주입될 값 (무엇을 꽂을 것인가)
-                // DependencyInjector 내부
-                Object value = beanFactory.getBean(constructor.getParameterTypes()[0].getName());
-                if (target != null && value != null) {
-                    constructor.setAccessible(true);
-                    constructor.newInstance(target,value);
-                    constructor.setAccessible(false);
-                }
 
+    // 생성자 주입 방법
+    public void constructorInject(Set<Constructor<?>> constructors) {
+        for (Constructor<?> constructor : constructors) {
+            try {
+                Class<?> clazz = constructor.getDeclaringClass();
+
+                // 생성자 파라미터 타입 조회
+                Object[] args = Arrays.stream(constructor.getParameterTypes()).map(type -> beanFactory.getBean(type.getName())).toArray();
+                constructor.setAccessible(true);
+                Object instance = constructor.newInstance(args);
+                beanFactory.registerBean(clazz.getName(), instance);
+                for (Class<?> iface : clazz.getInterfaces()) {
+                    beanFactory.registerBean(iface.getName(), instance);
+                }
+                constructor.setAccessible(false);
             } catch (Exception e) {
-                e.printStackTrace();
+                throw new IllegalStateException(constructor.getName() + "생성자 주입 중 오류 발생" + e.getMessage(), e);
             }
         }
     }
