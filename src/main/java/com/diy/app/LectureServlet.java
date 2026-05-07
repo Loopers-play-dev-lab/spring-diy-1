dd0ac93c0419002a5ad2d727ecab91e2af0b370e
river <bluuminn@gmail.com>
2026-05-07 12:10:01 +0900

    feat: POST /lectures 구현

--- src/main/java/com/diy/app/LectureServlet.java
+++ src/main/java/com/diy/app/LectureServlet.java
@@ -1,16 +1,22 @@
 package com.diy.app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ConcurrentHashMap;
 
 @WebServlet("/lectures")
 public class LectureServlet extends HttpServlet {
+
+    private static final ConcurrentHashMap<Long, Lecture> lectureMap = new ConcurrentHashMap<>();
 
     @Override
     public void init() throws ServletException {
@@ -27,16 +33,30 @@ public class LectureServlet extends HttpServlet {
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("[LectureServlet] doGet() is called.");
-        final Lecture loopakBeL2 = Lecture.of(1L, "Loop:PAK BE L2", BigDecimal.valueOf(1300000));
-        final Lecture springDiy = Lecture.of(2L, "Spring DIY", BigDecimal.valueOf(99000));
-        req.setAttribute("lectures", List.of(loopakBeL2, springDiy));
+        req.setAttribute("lectures", lectureMap.values());
         req.getRequestDispatcher("/lecture-list.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
+        System.out.println("[LectureServlet] doPost() is called.");
+        final var reader = new BufferedReader(new InputStreamReader(req.getInputStream(), StandardCharsets.UTF_8));
+        final var mapper = new ObjectMapper();
+        final var lectureRequest = mapper.readValue(reader.readLine(), LectureRequest.class);
+
+        final long lectureId = nextId();
+        lectureMap.put(lectureId, lectureRequest.toLecture(lectureId));
+        resp.sendRedirect("/lectures");
     }
 
     @Override
     public void destroy() {
         System.out.println("[LectureServlet] destroy() is called.");
         super.destroy();
+    }
+
+    private long nextId() {
+        return lectureMap.keySet().stream().mapToLong(Long::longValue).max().orElse(0L) + 1;
     }
 }
 
