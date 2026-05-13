cb0d033d0dba583d67ba340cd74111ecc1f6c064
cyPark95 <parkcy1007@gmail.com>
2026-05-13 21:50:14 +0900

    refactor: ApplicationContext 메서드 명명 및 구조 개선

--- src/main/java/com/diy/framework/context/ApplicationContext.java
+++ src/main/java/com/diy/framework/context/ApplicationContext.java
@@ -7,9 +7,11 @@ import com.diy.framework.context.annotation.Component;
 import com.diy.framework.web.mvc.Controller;
 import com.diy.framework.web.mvc.annotation.RequestMapping;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Executable;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -32,8 +34,8 @@ public class ApplicationContext {
     public void initialize() {
         BeanScanner beanScanner = new BeanScanner(basePackages);
         componentClasses.addAll(beanScanner.scanClassesTypeAnnotatedWith(Component.class));
-        componentClasses.forEach(this::registerBean);
-        componentClasses.forEach(this::registerBeanMethods);
+        componentClasses.forEach(this::registerConstructorBean);
+        componentClasses.forEach(this::registerMethodBean);
         componentClasses.forEach(this::registerHandlerMapping);
     }
 
@@ -42,22 +44,23 @@ public class ApplicationContext {
         if (bean == null) {
             throw new RuntimeException("Bean not found");
         }
+
         return bean;
     }
 
     public Map<String, Controller> getHandlerMap() {
-        return handlerMap;
+        return Collections.unmodifiableMap(handlerMap);
     }
 
-    private void registerBean(Class<?> clazz) {
-        String beanName = toBeanName(clazz);
+    private void registerConstructorBean(Class<?> clazz) {
+        String beanName = getBeanName(clazz);
         if (beans.containsKey(beanName)) {
             return;
         }
 
         if (clazz.isInterface()) {
             Class<?> implementClass = resolveImplementation(clazz);
-            registerBean(implementClass);
+            registerConstructorBean(implementClass);
             return;
         }
 
@@ -75,51 +78,22 @@ public class ApplicationContext {
         }
     }
 
-    private void registerBeanMethods(Class<?> clazz) {
-        Arrays.stream(clazz.getDeclaredMethods())
-                .filter(method -> method.isAnnotationPresent(Bean.class))
-                .forEach(method -> {
-                    String beanName = resolveBeanName(method);
-                    if (beans.containsKey(beanName)) {
-                        return;
-                    }
-                    try {
-                        Object factory = getBean(clazz);
-                        Object instance = method.invoke(factory);
-                        putBean(beanName, method.getReturnType(), instance);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-    }
+    private Constructor<?> resolveConstructor(Class<?> clazz) {
+        List<Constructor<?>> constructors = Arrays.stream(clazz.getDeclaredConstructors())
+                .filter(c -> c.isAnnotationPresent(Autowired.class))
+                .toList();
 
-    private void registerHandlerMapping(Class<?> clazz) {
-        if (!clazz.isAnnotationPresent(RequestMapping.class)) {
-            return;
+        if (constructors.isEmpty()) {
+            return Arrays.stream(clazz.getDeclaredConstructors())
+                    .filter(c -> c.getParameterCount() == 0)
+                    .findFirst()
+                    .orElse(clazz.getDeclaredConstructors()[0]);
+        }
+        if (constructors.size() > 1) {
+            throw new RuntimeException("Multiple @Autowired constructors");
         }
 
-        Object bean = beans.get(toBeanName(clazz));
-        if (bean instanceof Controller controller) {
-            String url = clazz.getAnnotation(RequestMapping.class).value();
-            handlerMap.put(url, controller);
-        }
-    }
-
-    private String resolveBeanName(Method method) {
-        String value = method.getAnnotation(Bean.class).value();
-        return value.isEmpty() ? method.getName() : value;
-    }
-
-    private void putBean(String beanName, Class<?> clazz, Object instance) {
-        beans.put(beanName, instance);
-        beanNamesByType.computeIfAbsent(clazz, c -> new ArrayList<>()).add(beanName);
-        Arrays.stream(clazz.getInterfaces())
-                .forEach(i -> beanNamesByType.computeIfAbsent(i, k -> new ArrayList<>()).add(beanName));
-    }
-
-    private String toBeanName(Class<?> clazz) {
-        String simpleName = clazz.getSimpleName();
-        return Character.toLowerCase(simpleName.charAt(0)) + simpleName.substring(1);
+        return constructors.getFirst();
     }
 
     private Class<?> resolveImplementation(Class<?> interfaceType) {
@@ -137,40 +111,72 @@ public class ApplicationContext {
         return implementations.getFirst();
     }
 
-    private Constructor<?> resolveConstructor(Class<?> clazz) {
-        List<Constructor<?>> constructors = Arrays.stream(clazz.getDeclaredConstructors())
-                .filter(c -> c.isAnnotationPresent(Autowired.class))
-                .toList();
-
-        if (constructors.isEmpty()) {
-            return Arrays.stream(clazz.getDeclaredConstructors())
-                    .filter(c -> c.getParameterCount() == 0)
-                    .findFirst()
-                    .orElse(clazz.getDeclaredConstructors()[0]);
-        }
-        if (constructors.size() > 1) {
-            throw new RuntimeException("Multiple @Autowired constructors");
-        }
-        return constructors.getFirst();
+    private void registerMethodBean(Class<?> clazz) {
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(Bean.class))
+                .forEach(method -> {
+                    String beanName = getBeanName(method);
+                    if (beans.containsKey(beanName)) {
+                        return;
+                    }
+                    try {
+                        Object factory = getBean(clazz);
+                        Object instance = method.invoke(factory, resolveArgs(method));
+                        putBean(beanName, method.getReturnType(), instance);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 
-    private Object[] resolveArgs(Constructor<?> constructor) {
-        return Arrays.stream(constructor.getParameterTypes())
+    private void registerHandlerMapping(Class<?> clazz) {
+        if (!clazz.isAnnotationPresent(RequestMapping.class)) {
+            return;
+        }
+
+        Object bean = beans.get(getBeanName(clazz));
+        if (bean instanceof Controller controller) {
+            String url = clazz.getAnnotation(RequestMapping.class).value();
+            handlerMap.put(url, controller);
+        }
+    }
+
+    private Object[] resolveArgs(Executable executable) {
+        return Arrays.stream(executable.getParameterTypes())
                 .map(type -> {
-                    registerBean(type);
+                    registerConstructorBean(type);
                     return getBean(type);
                 })
                 .toArray();
     }
 
+    private void putBean(String beanName, Class<?> clazz, Object instance) {
+        beans.put(beanName, instance);
+        beanNamesByType.computeIfAbsent(clazz, c -> new ArrayList<>()).add(beanName);
+        Arrays.stream(clazz.getInterfaces())
+                .forEach(i -> beanNamesByType.computeIfAbsent(i, k -> new ArrayList<>()).add(beanName));
+    }
+
+    private String getBeanName(Class<?> clazz) {
+        String simpleName = clazz.getSimpleName();
+        return Character.toLowerCase(simpleName.charAt(0)) + simpleName.substring(1);
+    }
+
+    private String getBeanName(Method method) {
+        String value = method.getAnnotation(Bean.class).value();
+        return value.isEmpty() ? method.getName() : value;
+    }
+
     private <T> T getBean(Class<T> type) {
         List<String> names = beanNamesByType.get(type);
+
         if (names == null || names.isEmpty()) {
             throw new RuntimeException("Bean not found");
         }
         if (names.size() > 1) {
             throw new RuntimeException("Multiple beans of same type");
         }
+
         return type.cast(beans.get(names.getFirst()));
     }
 }
