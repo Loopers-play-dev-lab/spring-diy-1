ec7ccd8ed153b4d44cb612c16c5665f218c19c95
portlogics-anewjean <anewjean@portlogics.com>
2026-05-20 00:50:54 +0900

    revert: BeanScanner 원복 및 테스트 스캔 범위를 실제 환경과 동일하게 변경

--- src/main/java/com/diy/framework/web/beans/factory/BeanScanner.java
+++ src/main/java/com/diy/framework/web/beans/factory/BeanScanner.java
@@ -1,10 +1,8 @@
 package com.diy.framework.web.beans.factory;
 
 import org.reflections.Reflections;
-import org.reflections.scanners.SubTypesScanner;
 
 import java.lang.annotation.Annotation;
-import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -13,29 +11,13 @@ public class BeanScanner {
     private final Reflections reflections;
 
     public BeanScanner(final String... basePackages) {
-        reflections = new Reflections(basePackages, new SubTypesScanner(false));
+        reflections = new Reflections((Object) basePackages);
     }
 
     public Set<Class<?>> scanClassesTypeAnnotatedWith(final Class<? extends Annotation> annotation) {
-        return reflections.getSubTypesOf(Object.class)
+        return reflections.getTypesAnnotatedWith(annotation)
                 .stream()
                 .filter(type -> (!type.isAnnotation() && !type.isInterface()))
-                .filter(type -> hasAnnotation(type, annotation))
                 .collect(Collectors.toSet());
-    }
-
-    private boolean hasAnnotation(Class<?> type, Class<? extends Annotation> target) {
-        return Arrays.stream(type.getAnnotations())
-                .anyMatch(a -> isOrContains(a.annotationType(), target));
-    }
-
-    private boolean isOrContains(Class<? extends Annotation> current, Class<? extends Annotation> target) {
-        if (current == target) {
-            return true;
-        }
-        return Arrays.stream(current.getAnnotations())
-                .map(Annotation::annotationType)
-                .filter(a -> !a.getPackageName().startsWith("java.lang"))
-                .anyMatch(a -> isOrContains(a, target));
     }
 }
