df331ac758a0555e8cd500a47225841c6b9da5e3
GOOHAESEUNG <amu6675@naver.com>
2026-05-19 19:31:33 +0900

    feat : @RequestMapping 메서드 레벨 지원 및 RequestMethod enum 추가

--- src/main/java/com/diy/framework/web/beans/RequestMapping.java
+++ src/main/java/com/diy/framework/web/beans/RequestMapping.java
@@ -1,13 +1,15 @@
 package com.diy.framework.web.beans;
 
+import com.diy.framework.web.annotation.RequestMethod;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RequestMapping {
-    String method() default "GET";
-    String uri();
+    String value() default "";
+    RequestMethod[] methods() default {};
 }
