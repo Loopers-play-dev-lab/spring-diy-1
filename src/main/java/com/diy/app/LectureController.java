82ec02cf945d3c1673c200a075b0622502e0f46b
river <bluuminn@gmail.com>
2026-05-19 18:10:47 +0900

    refactor: LectureController 리팩토링

--- src/main/java/com/diy/app/LectureController.java
+++ src/main/java/com/diy/app/LectureController.java
@@ -8,22 +8,12 @@ import org.jetbrains.annotations.NotNull;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class LectureController implements Controller {
 
-    private static final ConcurrentHashMap<Long, Lecture> lectureRepository = new ConcurrentHashMap<>();
-
-    static {
-        System.out.println("[LectureController] initialized.");
-        final Lecture loopakBeL2 = Lecture.of(1L, "Loop:PAK BE L2", BigDecimal.valueOf(1300000));
-        final Lecture springDiy = Lecture.of(2L, "Spring DIY", BigDecimal.valueOf(99000));
-        lectureRepository.put(1L, loopakBeL2);
-        lectureRepository.put(2L, springDiy);
-    }
+    private final LectureRepository lectureRepository = new LectureRepository();
 
     @Override
     public ModelAndView handleRequest(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
@@ -49,27 +39,23 @@ public class LectureController implements Controller {
 
     private ModelAndView doGet(final HttpServletRequest req, final HttpServletResponse resp) throws Exception {
         System.out.println("[LectureController] doGet() is called.");
-        return new ModelAndView("lecture-list", Map.of("lectures", lectureRepository.values()));
+        return new ModelAndView("lecture-list", Map.of("lectures", lectureRepository.findAll()));
     }
 
     private ModelAndView doPost(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
         System.out.println("[LectureController] doPost() is called.");
         final var lectureRequest = new ObjectMapper().readValue(readRequestBodyAsString(req), LecturePostRequest.class);
-        final long lectureId = nextId();
-        lectureRepository.put(lectureId, lectureRequest.toLecture(lectureId));
+        lectureRepository.save(lectureRequest.toNewLecture());
         return new ModelAndView("redirect:/lectures");
     }
-
 
     private ModelAndView doPut(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
         System.out.println("[LectureController] doPut() is called.");
         final var putRequest = new ObjectMapper().readValue(readRequestBodyAsString(req), LecturePutRequest.class);
-        final var lectureId = putRequest.getId();
-        final var target = Lecture.of(lectureId, putRequest.getName(), putRequest.getPrice());
-        lectureRepository.put(lectureId, target);
+        existsLecture(putRequest.getId());
+        lectureRepository.save(putRequest.toLecture());
         return new ModelAndView("redirect:/lectures");
     }
-
 
     private ModelAndView doDelete(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
         System.out.println("[LectureController] doDelete() is called.");
@@ -84,20 +70,21 @@ public class LectureController implements Controller {
         return new String(bytes, StandardCharsets.UTF_8);
     }
 
-    private long nextId() {
-        return lectureRepository.keySet().stream().mapToLong(Long::longValue).max().orElse(0L) + 1;
-    }
-
     private Long extractLectureId(final HttpServletRequest req) {
         final String lectureIdParam = req.getParameter("id");
         if (lectureIdParam == null || lectureIdParam.isBlank()) {
             throw new IllegalArgumentException("Lecture id is required.");
         }
-
         try {
             return Long.valueOf(lectureIdParam);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("Invalid lecture id.");
+        }
+    }
+
+    private void existsLecture(final Long lectureId) {
+        if (!lectureRepository.exists(lectureId)) {
+            throw new IllegalArgumentException("Lecture does not exist.");
         }
     }
 }
