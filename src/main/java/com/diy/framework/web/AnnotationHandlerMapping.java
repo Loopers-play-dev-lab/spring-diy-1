dd5fc023acd8420f5b0355259f8c156615a82064
GOOHAESEUNG <amu6675@naver.com>
2026-05-19 19:36:41 +0900

    feat : AnnotationHandlerMapping 및 HandlerExecution 구현

---
+++ src/main/java/com/diy/framework/web/AnnotationHandlerMapping.java
@@ -0,0 +1,46 @@
+package com.diy.framework.web;
+
+import com.diy.framework.web.annotation.Controller;
+import com.diy.framework.web.annotation.RequestMapping;
+import com.diy.framework.web.beans.factory.BeanFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AnnotationHandlerMapping {
+
+    private final Map<String, HandlerExecution> mappings = new HashMap<>();
+
+    public AnnotationHandlerMapping(BeanFactory beanFactory) {
+        initialize(beanFactory);
+    }
+
+    private void initialize(BeanFactory beanFactory) {
+        beanFactory.getBeans().forEach((clazz, bean) -> {
+            if (!clazz.isAnnotationPresent(Controller.class)) {
+                return;
+            }
+
+            String baseUri = clazz.isAnnotationPresent(RequestMapping.class)
+                    ? clazz.getAnnotation(RequestMapping.class).value()
+                    : "";
+
+            for (Method method : clazz.getDeclaredMethods()) {
+                if (!method.isAnnotationPresent(RequestMapping.class)) {
+                    continue;
+                }
+                RequestMapping rm = method.getAnnotation(RequestMapping.class);
+                String uri = baseUri + rm.value();
+                for (com.diy.framework.web.annotation.RequestMethod httpMethod : rm.methods()) {
+                    mappings.put(httpMethod.name() + ":" + uri, new HandlerExecution(bean, method));
+                }
+            }
+        });
+    }
+
+    public HandlerExecution getHandler(HttpServletRequest request) {
+        return mappings.get(request.getMethod() + ":" + request.getRequestURI());
+    }
+}
---
+++ src/main/java/com/diy/framework/web/HandlerExecution.java
@@ -0,0 +1,22 @@
+package com.diy.framework.web;
+
+import com.diy.framework.web.view.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+public class HandlerExecution {
+
+    private final Object instance;
+    private final Method method;
+
+    public HandlerExecution(Object instance, Method method) {
+        this.instance = instance;
+        this.method = method;
+    }
+
+    public ModelAndView handle(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        return (ModelAndView) method.invoke(instance, req, res);
+    }
+}
