a486628d36f52df52f18c05c858251f394448807
정인철 <jeong-incheol@s-MacBook.local>
2026-05-20 20:46:52 +0900

    refactor: DispatcherServlet HandlerMapping/Adapter 기반으로 변경

--- src/main/java/com/diy/framework/web/servlet/DispatcherServlet.java
+++ src/main/java/com/diy/framework/web/servlet/DispatcherServlet.java
@@ -1,58 +1,67 @@
 package com.diy.framework.web.servlet;
 
-import com.diy.framework.web.mvc.Controller;
-import com.diy.framework.web.mvc.view.JspViewResolver;
+import com.diy.framework.web.mvc.HandlerAdapter;
+import com.diy.framework.web.mvc.HandlerMapping;
 import com.diy.framework.web.mvc.view.ModelAndView;
-import com.diy.framework.web.mvc.view.UrlBasedViewResolver;
 import com.diy.framework.web.mvc.view.View;
 import com.diy.framework.web.mvc.view.ViewResolver;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
-@WebServlet("/")
 public class DispatcherServlet extends HttpServlet {
 
-    private final Map<String, Controller> controllersMapping;
-    private final List<ViewResolver> viewResolvers = new ArrayList<>();
+    private final List<HandlerMapping> handlerMappings;
+    private final List<HandlerAdapter> handlerAdapters;
+    private final List<ViewResolver> viewResolvers;
 
-    public DispatcherServlet(final Map<String, Controller> controllersMapping) {
-        this.controllersMapping = controllersMapping;
-        this.viewResolvers.add(new UrlBasedViewResolver());
-        this.viewResolvers.add(new JspViewResolver());
+    public DispatcherServlet(final List<HandlerMapping> handlerMappings,
+                             final List<HandlerAdapter> handlerAdapters,
+                             final List<ViewResolver> viewResolvers) {
+        this.handlerMappings = handlerMappings;
+        this.handlerAdapters = handlerAdapters;
+        this.viewResolvers = viewResolvers;
     }
 
     @Override
     protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-        final String uri = req.getRequestURI();
+        final Object handler = getHandler(req);
 
-        final Controller controller = controllersMapping.get(uri);
-
-        if (controller == null) {
+        if (handler == null) {
             return;
         }
 
         try {
-            final ModelAndView mav = controller.handleRequest(req, resp);
+            final HandlerAdapter adapter = getAdapter(handler);
+            final ModelAndView mav = adapter.handle(req, resp, handler);
             render(mav, req, resp);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
+    private Object getHandler(final HttpServletRequest req) {
+        return handlerMappings.stream()
+                .map(hm -> hm.getHandler(req))
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private HandlerAdapter getAdapter(final Object handler) {
+        return handlerAdapters.stream()
+                .filter(ha -> ha.supports(handler))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No HandlerAdapter found for handler: " + handler.getClass()));
+    }
+
     private void render(final ModelAndView mav, final HttpServletRequest req, final HttpServletResponse resp) throws Exception {
         final String viewName = mav.getViewName();
-
         final View view = resolveViewName(viewName);
 
         if (view == null) {
@@ -63,24 +72,12 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     private View resolveViewName(final String viewName) {
-        for (final ViewResolver viewResolver : this.viewResolvers) {
+        for (final ViewResolver viewResolver : viewResolvers) {
             final View view = viewResolver.resolveViewName(viewName);
             if (view != null) {
                 return view;
             }
         }
-
         return null;
-    }
-
-    private Map<String, ?> parseParams(final HttpServletRequest req) throws IOException {
-        if ("application/json".equals(req.getHeader("Content-Type"))) {
-            final byte[] bodyBytes = req.getInputStream().readAllBytes();
-            final String body = new String(bodyBytes, StandardCharsets.UTF_8);
-
-            return new ObjectMapper().readValue(body, new TypeReference<Map<String, Object>>() {});
-        } else {
-            return req.getParameterMap();
-        }
     }
 }
