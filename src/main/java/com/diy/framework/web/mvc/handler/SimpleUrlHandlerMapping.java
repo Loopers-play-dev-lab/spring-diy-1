cd7c1827569bc100d07b0700e37db048c435501f
정인철 <jeong-incheol@s-MacBook.local>
2026-05-27 20:08:23 +0900

    refactor: SimpleUrlHandlerMapping 빈 이름 기반 자체 초기화로 변경

--- src/main/java/com/diy/framework/web/mvc/handler/SimpleUrlHandlerMapping.java
+++ src/main/java/com/diy/framework/web/mvc/handler/SimpleUrlHandlerMapping.java
@@ -4,14 +4,19 @@ import com.diy.framework.web.mvc.Controller;
 import com.diy.framework.web.mvc.HandlerMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Map;
 
 public class SimpleUrlHandlerMapping implements HandlerMapping {
 
-    private final Map<String, Controller> handlerMap;
+    private final Map<String, Controller> handlerMap = new HashMap<>();
 
-    public SimpleUrlHandlerMapping(final Map<String, Controller> handlerMap) {
-        this.handlerMap = handlerMap;
+    public void initialize(final Map<String, Object> beans) {
+        beans.forEach((beanName, bean) -> {
+            if (bean instanceof Controller) {
+                handlerMap.put(beanName, (Controller) bean);
+            }
+        });
     }
 
     @Override
